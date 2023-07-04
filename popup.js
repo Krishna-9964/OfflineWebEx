c8da3ba420efec16f397f9c44421d53906cdf2e9
Krishna-9964 <kbkrishnakitty99@gmail.com>
2023-07-04 10:46:07 +0530

    fitst changes

--- popup.js
+++ popup.js
@@ -4,7 +4,6 @@ button.addEventListener('click', function () {
     window.open("index.html", '_blank')
 
 })
-
 filter = document.getElementById('FilterHistory');
 filter.addEventListener('click', function () {
     window.open("filter.html", '_blank')
@@ -14,5 +13,4 @@ filter.addEventListener('click', function () {
 search = document.getElementById('OfflineSearch');
 search.addEventListener('click', function () {
     window.open("./search box/index.html", '_blank')
-
 })
